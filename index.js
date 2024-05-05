729dd8fb42bcf7050aa1aa44146dca57f5bb4e0a
Ashok Kumar Karri <ashok.ashok1999@gmail.com>
2024-05-05 18:53:01 +0530

    hello bro

---
+++ index.js
@@ -0,0 +1,10 @@
+var _=require('underscore');
+
+// Code modules 
+// FIle or folder  
+// node_modules 
+
+var result =_.contains([1,2,3],2);
+console.log(result);
+
+////we can ingonre file while ignoring 
